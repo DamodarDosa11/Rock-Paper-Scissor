08aba7ae871c0a19dc83663f99828f473592938a
Damodar <damodardosa11@outlook.com>
2025-02-27 13:01:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const gameContainer = document.querySelector(".container"),
+userResult = document.querySelector(".user_result img"),
+cpuResult = document.querySelector(".cpu_result img"),
+result = document.querySelector(".result"),
+optionImages = document.querySelectorAll(".option_image");
+
+optionImages.forEach((image,index) =>{
+    image.addEventListener("click",(e) => {
+        image.classList.add("active");
+        optionImages.forEach((image2,index2) => {
+            index !== index2 && image2.classList.remove("click");
+            let imageSrc = e.target.querySelector("img").src;
+            userResult.src = imageSrc;
+            let randomNumber = Math.floor(Math.random() * 3);
+            let cpuImages = ["./images/rock.png","./images/paper.png","./images/scissors.png"];
+            cpuResult.src = cpuImages[randomNumber];
+            let cpuValue = ["R","P","S"][randomNumber];
+            let userValue = ["R","P","S"][index];
+            
+            let outComes = {
+                RR:"Draw",
+                RS:"Cpu",
+                RP:"User",
+                PP:"Draw",
+                PS:"User",
+                PR:"Cpu",
+                SS:"Draw",
+                SR:"User",
+                SP:"Cpu",
+            };
+            let outComeValue = outComes[userValue + cpuValue];
+            result.textContent = userValue === cpuValue ? "Match Draw" : `${outComeValue} Won!!`;
+        });
+    });
+});
